2234051d258a5e9c2ba0006b1b1807b23f429939
Roshan Lakmal <roshanrox911@gmail.com>
2018-02-05 16:12:06 +0530

    Exercise

---
+++ IterationFlow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IterationFlow
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //1 - Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.
+            //Display the count on the console.
+            int counter = 0;
+            for (int i = 1; i <= 100;i++){
+                if(i%3==0){
+                    counter++;
+                }
+            }
+            Console.WriteLine(string.Format("Numbers that are divisible by 3 are : {0}", counter));
+        }
+    }
+}
